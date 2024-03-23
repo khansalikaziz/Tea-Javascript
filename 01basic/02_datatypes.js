0e50962e969ccee035e4d473836ef30840a7e7b6
Salik Aziz Khan <khansalikaziz786@gmail.com>
2024-03-23 16:37:20 +0000

    datatypes

---
+++ 01basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node js , not browser
+
+let name="Salik"
+let age = 18
+let isLoggedIn= false
+
+// number => 
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value type = object
+// undefined => type undefined
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
