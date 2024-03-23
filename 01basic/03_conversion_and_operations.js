08855b3b002f73a3fe143688fa0121dafc09616a
Salik Aziz Khan <khansalikaziz786@gmail.com>
2024-03-23 17:06:51 +0000

    conversion of datatype

---
+++ 01basic/03_conversion_and_operations.js
@@ -0,0 +1,31 @@
+let score = null
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.table([valueInNumber,typeof(valueInNumber)])
+
+// "33" ==> 33
+// "33abc" ==> NaN
+// true ==> 1
+// false ==> 0
+// null ==> 0
+// undefined ==> NaN
+
+let isLoggedIn =undefined
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) 
+
+// console.table([booleanIsLoggedIn,typeof booleanIsLoggedIn])
+
+// 1 ==> true
+// 0 ==> false
+// "" ==> false
+// "salik" ==> true
+// null ==> false
+// undefined ==> false
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber,typeof(stringNumber))
