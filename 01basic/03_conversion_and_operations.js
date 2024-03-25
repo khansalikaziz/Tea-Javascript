dc12564d95375fef633910f6cb399a684e527ed1
Salik Aziz Khan <khansalikaziz786@gmail.com>
2024-03-25 16:19:48 +0000

    Pending changes exported from your codespace

--- 01basic/03_conversion_and_operations.js
+++ 01basic/03_conversion_and_operations.js
@@ -28,4 +28,30 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber,typeof(stringNumber))
+
+// console.log(stringNumber,typeof(stringNumber))
+
+// **************   Operations  **************
+let value = 3
+let negValue= -value
+//console.log(negValue)
+
+
+let str1 = "hello"
+let str2 = " Salik"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log("1"+2+2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1= num2 = num3 = 2+2
+console.log(num2);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
